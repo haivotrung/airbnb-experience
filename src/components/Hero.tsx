14f254466ae7babe6536b75e11e7df190d209b26
Hai Vo <haivotrung@gmail.com>
2022-03-09 19:58:33 -0500

    changed width for hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,8 @@ export default function Hero() {
         <section className="max-w-5xl p-10 m-auto">
             <img className="" src={require("./images/photo-grid.png")} alt="hero grid" />
             <div className="pt-9">
-                <h1 className="w-9/12 text-5xl font-semibold">Online Experiences</h1>
-                <p className="w-7/12 pt-5 text-xl font-semilight">Join unique interactive activities led by
+                <h1 className="w-8/12 text-5xl font-semibold">Online Experiences</h1>
+                <p className="w-10/12 pt-5 text-xl md:w-6/12 font-semilight">Join unique interactive activities led by
                     one-of-a-kind hosts—all without leaving home.</p>
             </div>
         </section>
