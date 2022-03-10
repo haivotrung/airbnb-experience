880f2bbf671fc4046b55dab0fd698aa058d8e21b
Hai Vo <haivotrung@gmail.com>
2022-03-09 19:20:16 -0500

    updated responsive for large screen

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -32,30 +32,29 @@ export default function Card(props: {
     openSpots: number;
 }) {
     return (
-        <div className="flex-none w-2/3 mr-8 border rounded-lg md:w-1/3 md:pb-4">
+        <div className="flex-none w-3/5 mr-8 border rounded-lg md:w-1/3 md:pb-4 lg:w-1/5">
             <div className="relative overflow-hidden rounded">
-
-                <img className="w-full rounded-lg" src={require(`./images/${props.coverImg}`)} alt={props.title} />
+                <img className="w-screen rounded-lg" src={require(`./images/${props.coverImg}`)} alt={props.title} />
 
                 <div className="absolute top-0 items-center mt-2.5 mb-5">
                     <span className="bg-white text-black text-xs font-light mr-2 px-2.5 py-0.5 rounded ml-3">{(props.openSpots === 0) ? `SOLD OUT` : `${props.openSpots} left`}</span>
                 </div>
+            </div>
 
-                <div className="p-2 pl-2">
-                    <div className="pt-2">
-                        <div className="flex items-center space-x-2 justify-self-start">
-                            <img className="h-3" src={StarIcon} alt="star icon" />
-                            <p>{props.stats.rating}</p>
-                            <p className="text-gray-400">({props.stats.reviewCount}) •</p>
-                            <p className="text-gray-400">{props.location}</p>
-                        </div>
+            <div className="p-2 pl-2">
+                <div className="pt-2">
+                    <div className="flex items-center space-x-2 justify-self-start">
+                        <img className="h-3" src={StarIcon} alt="star icon" />
+                        <p>{props.stats.rating}</p>
+                        <p className="text-gray-400">({props.stats.reviewCount}) •</p>
+                        <p className="text-gray-400">{props.location}</p>
+                    </div>
 
-                        <h5 className="tracking-tight text-black">{props.title}</h5>
+                    <h5 className="tracking-tight text-black">{props.title}</h5>
 
-                        <div className="flex">
-                            <h3 className="text-sm font-bold">From ${props.price}</h3>
-                            <p className="text-sm text-gray-500">&nbsp; / per person</p>
-                        </div>
+                    <div className="flex">
+                        <h3 className="text-sm font-bold">From ${props.price}</h3>
+                        <p className="text-sm text-gray-500">&nbsp; / per person</p>
                     </div>
                 </div>
             </div>
